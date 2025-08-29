a63d122e84a9f7245d631d7fb2e9339b3a727346
Ivan <i.cabellos@id.uib.eu>
2025-08-29 17:46:09 +0200

    updated wsgi

--- api/wsgi.py
+++ api/wsgi.py
@@ -1,4 +1,11 @@
 import os
 from django.core.wsgi import get_wsgi_application
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api.settings")
+
+# WSGI estándar de Django
 application = get_wsgi_application()
+
+# 👇 Alias para Vercel (necesita `app` o `handler`)
+app = application
+handler = application
