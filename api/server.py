d90d0b3d052bff580d039e25dfffac92a76415e6
Ivan <i.cabellos@id.uib.eu>
2025-08-29 18:04:10 +0200

    added new requirements for deploying

--- api/server.py
+++ api/server.py
@@ -4,5 +4,6 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api.settings")
 
-# Vercel busca exactamente `app` o `handler` a nivel módulo:
-app = get_wsgi_application()
+application = get_wsgi_application()
+app = application
+handler = application
